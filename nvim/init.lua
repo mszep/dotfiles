be16cc429974cd691129c4eb9174e9e6651c5703
Mark Szepieniec <mszepieniec@xyme.ai>
2025-07-29 14:44:07 +0100

    move LSP keymaps to own dir

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,22 +38,6 @@ end
 vim.keymap.set("n", "<C-l>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
 
-local autocmd = vim.api.nvim_create_autocmd
-autocmd('LspAttach', {
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
-    end
-})
 
 
 vim.g.netrw_browse_split = 0
