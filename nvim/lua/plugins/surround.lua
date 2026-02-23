7755e0dda6a8a29a6d661aa488f452567c91ac50
Mark Szepieniec <mszepieniec@xyme.ai>
2026-02-23 14:59:02 +0000

    fix surround.vim config

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -5,19 +5,6 @@ return {
   config = function()
     require("nvim-surround").setup({
       -- Configuration here, or leave empty to use defaults
-      keymaps = {
-        insert = "<C-g>s",
-        insert_line = "<C-g>S",
-        normal = "ys",
-        normal_cur = "yss",
-        normal_line = "yS",
-        normal_cur_line = "ySS",
-        visual = "S",
-        visual_line = "gS",
-        delete = "ds",
-        change = "cs",
-        change_line = "cS",
-      },
       surrounds = {
         -- Custom surrounds can be added here
         -- Example: Function calls
